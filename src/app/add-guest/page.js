ef04512cb11226792b3976d8475539bf7541e5d8
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-06-26 16:14:40 +0530

    add-guest fix

--- src/app/add-guest/page.js
+++ src/app/add-guest/page.js
@@ -38,7 +38,7 @@ const Page = () => {
       <div className="container">
         <div className="py-[40px] 2xl:py-[120px] flex items-stretch">
           <div className="bg-[#F7F7F7] p-[40px] 2xl:p-[60px] rounded-2xl w-[60%] 3xl:w-[914px]">
-            <h3 className="text-[30px] font-semibold text-[#151515] mb-[43px]">
+            <h3 className="3xl:text-[30px] font-semibold text-[#151515] mb-[43px]">
               Add guest
             </h3>
 
@@ -108,7 +108,7 @@ const Page = () => {
                 </div>
               </div>
               <div className="flex items-center">
-                <div className="flex flex-col w-full mr-7">
+                <div className="flex flex-col w-full">
                   <label
                     htmlFor="address"
                     className="font-normal text-[#151515] text-[18px]"
@@ -147,7 +147,7 @@ const Page = () => {
                     <option value="guest 4">guest 4</option>
                   </select>
                 </div>
-                <div className="flex flex-col w-full mr-7">
+                <div className="flex flex-col w-full ">
                   <label
                     htmlFor="dietary"
                     className="font-normal text-[#151515] text-[18px]"
@@ -171,7 +171,7 @@ const Page = () => {
                 </div>
               </div>
               <div className="flex items-center">
-                <div className="flex flex-col w-full mr-7">
+                <div className="flex flex-col w-full">
                   <label
                     htmlFor="notes"
                     className="font-normal text-[#151515] text-[18px]"
@@ -186,13 +186,13 @@ const Page = () => {
                   ></textarea>
                 </div>
               </div>
-              <button className="cursor-pointer transition font-semibold text-[20px] text-white py-3.5 bg-[#EA0056] hover:bg-[#c9004a] rounded-lg w-[317px] mx-auto table">
+              <button className="cursor-pointer transition font-semibold text-[15px] 2xl:text-[16px] 3xl:text-[20px] text-white py-3.5 bg-[#EA0056] hover:bg-[#c9004a] rounded-lg w-[317px] mx-auto table">
                 Add to list
               </button>
             </form>
           </div>
           <div className="bg-[#F7F7F7] p-[40px] 2xl:p-[60px] rounded-2xl w-[40%] 3xl:w-[calc(100%-914px)] ml-[30px] 3xl:ml-[40px] flex flex-col">
-            <h3 className="text-[30px] font-semibold text-[#151515] mb-[43px]">
+            <h3 className="text-[24px] 3xl:text-[30px] font-semibold text-[#151515] mb-[43px]">
               Guest list
             </h3>
 
@@ -241,7 +241,7 @@ const Page = () => {
 
               <button
                 type="submit"
-                className="cursor-pointer mt-auto font-semibold text-[20px] text-white py-3.5 bg-[#EA0056] hover:bg-[#c9004a] transition rounded-lg w-[317px] mx-auto table"
+                className="cursor-pointer mt-auto font-semibold text-[15px] 2xl:text-[16px] 3xl:text-[20px] text-white py-3.5 bg-[#EA0056] hover:bg-[#c9004a] transition rounded-lg w-[317px] mx-auto table"
               >
                 Submit all
               </button>
