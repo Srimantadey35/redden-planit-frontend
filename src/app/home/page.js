cf2bae2aac7c96ed77bd2d9cfe26a626059a68c6
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-06-30 11:55:23 +0530

    minor fixes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,20 +1,21 @@
-'use client'
+"use client";
 import Header from "@/components/Header";
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import AddMultipleGuests from "@/components/widgets/AddMultipleGuests";
 
 const page = () => {
-  
   const [isMultipleGuestsOpen, setisMultipleGuestsOpen] = useState(false);
-
+  const [isModalOpen, setisModalOpen] = useState(null);
+  console.log(isModalOpen);
   const cardData = [
     {
       img: "/images/home/add.svg",
       cardName: "Add Guest Manually",
       cardDesc: "Type your guests' contact info directly onto your guest list.",
       btnText: "Add a guest",
-      href:'/add-guest'
+      href: "/add-guest",
     },
     {
       img: "/images/home/list.svg",
@@ -22,7 +23,7 @@ const page = () => {
       cardDesc:
         "Already have a list? Use a spreadsheet to quickly add multiple guests.",
       btnText: "Add multiple guests",
-      href:'/add-guest'
+      href: "/add-guest",
     },
     {
       img: "/images/home/gmail.svg",
@@ -30,17 +31,20 @@ const page = () => {
       cardDesc:
         "Import from you Gmail to add the guest list synced from your contacts.",
       btnText: "Import",
-      href:'/add-guest'
+      href: "/add-guest",
     },
   ];
   return (
     <div className="bg-white min-h-screen">
       <Header />
       <div className="container">
-        <div className="py-[40px] 2xl:py-[120px]">
+        <div className="py-[40px] 2xl:py-[60px] 3xl:py-[120px]">
           <h3 className="text-center font-medium text-[27px] 2xl:text-[30px] text-[#151515]">
             Hi{" "}
-            <span className="text-black text-[44px] 3xl:text-[50px]" style={{fontFamily:'allura-font'}}>
+            <span
+              className="text-black text-[44px] 3xl:text-[50px]"
+              style={{ fontFamily: "allura-font" }}
+            >
               Rabina
             </span>
           </h3>
@@ -51,8 +55,11 @@ const page = () => {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[16px] lg:gap-[24px] xl:gap-[40px] mt-8 sm:mt-12 md:mt-16 2xl:mt-[75px]">
             {cardData.map((item, index) => (
-              <div key={index} className="bg-[#F7F7F7] rounded-2xl p-[28px] xl:p-[50px] hover:scale-[1.03] transition ease-in-out duration-300 hover:shadow-xl flex flex-col">
-                <div className="mx-auto sm:mx-0 size-[45px] lg:size-[50px] 2xl:size-[74px] rounded-full bg-white grid place-items-center p-3.5">
+              <div
+                key={index}
+                className="bg-[#F7F7F7] rounded-2xl p-[28px] xl:p-[50px] hover:scale-[1.03] transition ease-in-out duration-300 hover:shadow-xl flex flex-col"
+              >
+                <div className="mx-auto sm:mx-0 size-[45px] lg:size-[50px] 2xl:size-[55px] 3xl:size-[74px] rounded-full bg-white grid place-items-center p-3.5">
                   <Image width={38} height={38} src={item.img} alt={item.img} />
                 </div>
                 <h3 className="text-[#151515] sm:text-left text-center font-semibold text-[18px] lg:text-[18px] xl:text-[22px] 3xl:text-[30px] mt-2">
@@ -61,14 +68,25 @@ const page = () => {
                 <p className="sm:text-left text-center leading-[1.2] text-[18px] 3xl:text-[20px] font-normal text-[#212121] mt-5 mb-6">
                   {item.cardDesc}
                 </p>
-                <Link href={`${item.href}`} className="text-center font-semibold cursor-pointer text-[15px] 3xl:text-[20px] text-white bg-[#EA0056] hover:bg-[#c9004a] rounded-lg py-2 xl:py-2.5 2xl:py-3 3xl:py-3.5 px-3 w-full mt-auto">
-                  {item.btnText}
-                </Link>
+                {item.btnText === "Add a guest" ? (
+                  <Link
+                    href={`${item.href}`}
+                    className="text-center font-semibold cursor-pointer text-[15px] 3xl:text-[20px] text-white bg-[#EA0056] hover:bg-[#c9004a] rounded-lg py-2 xl:py-2.5 2xl:py-3 3xl:py-3.5 px-3 w-full mt-auto"
+                  >
+                    {item.btnText}
+                  </Link>
+                ) : (
+                  <button onClick={()=>setisModalOpen(item.btnText)} className="text-center font-semibold cursor-pointer text-[15px] 3xl:text-[20px] text-white bg-[#EA0056] hover:bg-[#c9004a] rounded-lg py-2 xl:py-2.5 2xl:py-3 3xl:py-3.5 px-3 w-full mt-auto">
+                    {item.btnText}
+                  </button>
+                )}
               </div>
             ))}
           </div>
         </div>
       </div>
+
+      {/* <AddMultipleGuests/> */}
     </div>
   );
 };
--- src/components/widgets/AddMultipleGuests.js
+++ src/components/widgets/AddMultipleGuests.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const AddMultipleGuests = () => {
   return (
-    <div>
-        <h3 className='font-semibold text-[30px] text-[#151515]'>Add multiple guests</h3>
+    <div className="fixed top-0 left-0 bg-[#000000a5] z-[99999] size-full">
+      <div className="flex items-center justify-center size-full">
+        <div className="bg-white rounded-[15px] relative">
+          <div className="px-[50px] mt-[55px] pb-[20px]">
+            <div className="flex items-center mb-[50px]">
+              <h3 className="font-semibold text-[30px] text-[#151515]">
+                Add multiple guests
+              </h3>
+              <Image
+                width={25}
+                height={25}
+                src={"/images/cross.svg"}
+                alt="cross icon"
+              />
+            </div>
+            <div>
+              <h4 className="text-[#151515] font-medium text-[22px]">
+                Upload a Spreadsheet
+              </h4>
+            </div>
+          </div>
+          <div className="border-t border-t-[#DFDFDF] pt-[35px] px-[50px]">
+           <p className="text-[#151515] font-medium text-[18px]">Follow the guidelines below to adapt an existing spreadsheet.</p>
+           <ul className="pt-[20px]">
+            <li className="font-normal text-[#505050] text-[16px]">Enter column headers (eg. Name, Plus One, Street Address) in the first row.</li>
+           </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddMultipleGuests
+export default AddMultipleGuests;
